e7b99cd3f27ec31a43cc7c486e1680ae4f98dad9
ashwinyadav09 <ydashwin09@gmail.com>
2025-02-04 04:01:21 +0530

    update

--- login.py
+++ login.py
@@ -3,6 +3,24 @@ import json
 import requests
 from database import *
 
+import sqlite3
+import requests
+
+def delete_existing_user(email):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    # Check if the email exists in the users table
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    existing_user = cursor.fetchone()
+
+    # If the user exists, delete the record
+    if existing_user:
+        cursor.execute("DELETE FROM users WHERE email = ?", (email,))
+        conn.commit()
+
+    conn.close()
+
 def login_and_save_data(email: str, password: str):
     login_url = "https://student.bennetterp.camu.in/login/validate"
     payload = {
@@ -26,7 +44,11 @@ def login_and_save_data(email: str, password: str):
         data = resp_data["output"]["data"]
         if "logindetails" in data:
             # Login successful
-            email_prefix = email.split("@")[0]  # Extract the part before @ from the email
+            delete_existing_user(email)
+            email_prefix = email.split("@")[0].upper() # Extract the part before @ from the email
+            email_end = email.split("@")[1]
+            email = email_prefix + "@" + email_end
+            delete_existing_user(email)
             new_user(name=data["logindetails"]["Name"],reg_no=email_prefix,email=email,password=password)
             store_js(resp_data,email)
 
