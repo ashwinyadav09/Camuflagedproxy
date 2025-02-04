5f1f8ee51bcbc7227c457aa9d99d353a502caab8
ashwinyadav09 <ydashwin09@gmail.com>
2025-02-04 18:36:32 +0530

    update

--- markit.py
+++ markit.py
@@ -1,6 +1,4 @@
 import requests
-import sqlite3
-from concurrent.futures import ThreadPoolExecutor
 from sid import *
 from database import *
 
@@ -23,31 +21,37 @@ def mark_attendance(session_id: str, attendance_id: str, student_id: str):
 
     response = requests.post(url, headers=headers, json=payload)
     try:
-        response = response.json()  # Return the response as JSON
-        if response.get("output", {}).get("data") is not None:
-            code = response["output"]["data"].get("code")
-            if code == "ATTENDANCE_ALREADY_MARKED" or code == "SUCCESS":
-                return True
+        response=response.json()  # Return the response as JSON
+        try:
+            if response["output"]["data"]!=None:
+                #print("Attendance Response:", response["output"]["data"]["code"])
+                if response["output"]["data"]["code"]=="ATTENDANCE_ALREADY_MARKED" or response["output"]["data"]["code"]=="SUCCESS":
+                    return True
+                else:
+                    return False
             else:
+                #print("Failed to mark attendance.")
                 return False
-        return False
+        except Exception as e:
+            print(e)
+            return False
     except ValueError:
         print("Failed to parse response as JSON.")
         return False
 
-def start_mark(qr_id: str):
+
+import sqlite3
+from qr import *
+def start_mark(qr_id:str):
     conn = sqlite3.connect('database.db')
-    c = conn.cursor()
+    c=conn.cursor()
     c.execute('SELECT email FROM users')
-    attendance_id = qr_id
-    ems = c.fetchall()
-
-    def mark_for_user(em):
-        sid = get_sid(em, get_pass(em))
+    attendance_id=qr_id
+    ems=c.fetchall()
+    l=[]
+    for em in ems:
+        em=em[0]
+        sid = get_sid(em,get_pass(em))
         student_id = get_stu(em)
-        return mark_attendance(sid, attendance_id, student_id)
-
-    with ThreadPoolExecutor() as executor:
-        results = list(executor.map(mark_for_user, [em[0] for em in ems]))
-
-    return results
+        l.append(mark_attendance(sid, attendance_id, student_id))
+    return l
